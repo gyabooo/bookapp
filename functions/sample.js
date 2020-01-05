79290721892ed3788f19e900948ad2be958fcdc5
seita_matsumoto <gyabooo@gmail.com>
2020-01-05 14:15:09 +0900

    update

--- functions/sample.js
+++ functions/sample.js
@@ -1,8 +1,6 @@
-export async function handler(event, context) {
-  return {
+exports.handler = function(event, context, callback) {
+  callback(null, {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello world ${Math.floor(Math.random() * 10)}`
-    })
-  };
-}
+    body: "Hello, World"
+  });
+};
